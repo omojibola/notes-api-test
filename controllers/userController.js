ced325dddbc6dca05c53350745c5cac2165c2a24
Jibola <omojibolaorija@gmail.com>
2023-04-22 14:02:28 +0100

    first

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,7 +1,7 @@
-const asyncHandler = require("express-async-handler");
-const bcrypt = require("bcrypt");
-const jwt = require("jsonwebtoken");
-const User = require("../models/userModel");
+const asyncHandler = require('express-async-handler');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const User = require('../models/userModel');
 
 //@desc Register a user
 //@route POST /api/users/register
@@ -10,31 +10,31 @@ const registerUser = asyncHandler(async (req, res) => {
   const { username, email, password } = req.body;
   if (!username || !email || !password) {
     res.status(400);
-    throw new Error("All fields are mandatory!");
+    throw new Error('All fields are mandatory!');
   }
   const userAvailable = await User.findOne({ email });
   if (userAvailable) {
     res.status(400);
-    throw new Error("User already registered!");
+    throw new Error('User already registered!');
   }
 
   //Hash password
   const hashedPassword = await bcrypt.hash(password, 10);
-  console.log("Hashed Password: ", hashedPassword);
   const user = await User.create({
     username,
     email,
     password: hashedPassword,
   });
 
-  console.log(`User created ${user}`);
   if (user) {
-    res.status(201).json({ _id: user.id, email: user.email });
+    res
+      .status(201)
+      .json({ id: user.id, email: user.email, username: user.username });
   } else {
     res.status(400);
-    throw new Error("User data us not valid");
+    throw new Error('User data is not valid');
   }
-  res.json({ message: "Register the user" });
+  res.json({ message: 'Register the user' });
 });
 
 //@desc Login user
@@ -44,7 +44,7 @@ const loginUser = asyncHandler(async (req, res) => {
   const { email, password } = req.body;
   if (!email || !password) {
     res.status(400);
-    throw new Error("All fields are mandatory!");
+    throw new Error('All fields are mandatory!');
   }
   const user = await User.findOne({ email });
   //compare password with hashedpassword
@@ -58,12 +58,17 @@ const loginUser = asyncHandler(async (req, res) => {
         },
       },
       process.env.ACCESS_TOKEN_SECERT,
-      { expiresIn: "15m" }
+      { expiresIn: '15m' }
     );
-    res.status(200).json({ accessToken });
+    res.status(200).json({
+      id: user.id,
+      email: user.email,
+      username: user.username,
+      accessToken,
+    });
   } else {
     res.status(401);
-    throw new Error("email or password is not valid");
+    throw new Error('email or password is not valid');
   }
 });
 
--- server.js
+++ server.js
@@ -1,16 +1,20 @@
-const express = require("express");
-const connectDb = require("./config/dbConnection");
-const errorHandler = require("./middleware/errorHandler");
-const dotenv = require("dotenv").config();
+const express = require('express');
+const connectDb = require('./config/dbConnection');
+const errorHandler = require('./middleware/errorHandler');
+const dotenv = require('dotenv').config();
 
 connectDb();
 const app = express();
 
 const port = process.env.PORT || 5000;
 
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
+
 app.use(express.json());
-app.use("/api/contacts", require("./routes/contactRoutes"));
-app.use("/api/users", require("./routes/userRoutes"));
+app.use('/api/contacts', require('./routes/contactRoutes'));
+app.use('/api/users', require('./routes/userRoutes'));
 app.use(errorHandler);
 
 app.listen(port, () => {
