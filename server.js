aba58f67dd4711f2feb76503983d0f2c9cd5d0e8
Jibola <omojibolaorija@gmail.com>
2023-04-27 18:58:13 +0100

    fixed cors

--- server.js
+++ server.js
@@ -2,9 +2,16 @@ const express = require('express');
 const connectDb = require('./config/dbConnection');
 const errorHandler = require('./middleware/errorHandler');
 const dotenv = require('dotenv').config();
+const cors = require('cors');
 
 connectDb();
 const app = express();
+app.use(
+  cors({
+    credentials: true,
+    origin: ['http://127.0.0.1:5173'],
+  })
+);
 
 const port = process.env.PORT || 5000;
 
